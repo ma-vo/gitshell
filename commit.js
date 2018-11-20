cc4b411113ce86873a8352a84460198eda3d94d1
Marc Vongerichten <marc.vongerichten@publicispixelpark.de>
2018-11-20 14:42:51 +0100

    feat(error handling) add working error handling

--- commit.js
+++ commit.js
@@ -17,12 +17,19 @@ const addCommitMsg = (commit) => {
     const commitMessage = `${filterTypeMsg(commit.type)}(${commit.scope}${commit.ticket ? ':' + commit.ticket : ''}) ${commit.subject}`;
 
     shell.exec(`git commit -m "${commitMessage}"`);
-    notificationOk(commitMessage);
+    if (!shell.error()) {
+        notificationOk(commitMessage);  
+    } else {
+        const errorMsg = 'Sorry, an error occured.';
+        shell.echo(`${errorMsg} Look above for more details.`);
+        notificationError(`${errorMsg} Look in console for more details.`);
+    }
 };
 
 /**
  * @function  [filterTypeMsg]
  * @returns {String} filteredChoice
+ * @description removes unnecessary explanation string after listitem. f.e. "feat (feature)" -> "feat"
  */
 const filterTypeMsg = (choice) =>
     choice.replace(/ *\([^)]*\) */g, "");
@@ -36,7 +43,7 @@ const notificationOk = (msg) =>
         {
             title: 'Git Commit',
             message: msg,
-            icon: path.join(__dirname, 'assets/git-ok.png'), // Absolute path (doesn't work on balloons)
+            icon: path.join(__dirname, 'assets/git-ok.png'),
             sound: false, // Only Notification Center or Windows Toasters
             wait: false // Do not wait for user action
         }
@@ -51,7 +58,7 @@ const notificationError = (msg) =>
         {
             title: 'Git Commit Error',
             message: msg,
-            icon: path.join(__dirname, 'assets/git-error.png'), // Absolute path (doesn't work on balloons)
+            icon: path.join(__dirname, 'assets/git-error.png'),
             sound: true, // Only Notification Center or Windows Toasters
             wait: false // Do not wait for user action
         }
